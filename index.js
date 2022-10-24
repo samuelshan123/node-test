10f493d47e3e2a3cb62f360f08f2983a6794c9b1
samuel-retroacto <samuelraj.raje@retroacto.com>
2022-10-24 12:17:45 +0530

    port changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.get("/test", (req, res) => {
   
@@ -9,5 +9,5 @@ app.get("/test", (req, res) => {
 
 
   app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at ${port}`);
   });
